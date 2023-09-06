83323ac99d165c64dbf27463c58fdc4f8c08d675
KauaLealZ <kaua.santos@sptech.school>
2023-09-06 16:05:51 -0300

    Index finalizada

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: true,
+      dots: true,
+      prevArrow: '<button class="slick-prev" aria-label="Previous" type="button">Previous</button>',
+      nextArrow: '<button class="slick-next" aria-label="Next" type="button">Next</button>'
+    });
+  });
