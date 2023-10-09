5ce4817b362652ad83a95ee3b34d46db74603d53
Diego <diegosousa241104@gmail.com>
2023-10-08 22:40:34 -0300

    feat: Adicionando graficos na tela de visao especifica do totem

---
+++ site/public/js/visaoEspecifica/hd.js
@@ -0,0 +1,89 @@
+var ctxHD = document.getElementById("barChartHD").getContext("2d");
+
+var labelsHD = ["Seg", "Ter", "Qua", "Qui", "Sex", "Sab", "Dom"];
+var dataHD = {
+    labels: labelsHD,
+    datasets: [
+        {
+            label: 'Disco Local (C:)',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            borderColor: "#BD06DD",
+            tension: .4,
+            fill: true,
+            backgroundColor: '#BD06DD',
+            borderRadius: 10,
+        },
+        {
+            label: 'Disco Local (D:)',
+            data: [32, 51, 10, 81, 90, 65, 25],
+            borderColor: "#8D11A4",
+            tension: .4,
+            fill: true,
+            backgroundColor: '#8D11A4',
+            borderRadius: 10,
+        },
+    ],
+};
+
+var HDChart = new Chart(ctxHD, {
+    type: "bar",
+    data: dataHD,
+    options: {
+        plugins: {
+            legend: {
+                position: 'bottom',
+                labels: {
+                    color: '#FFF',
+                    font: {
+                        family: 'Montserrat',
+                    }
+                }
+            }
+        },
+        tooltips: {
+            enabled: false,
+        },
+        scales: {
+            x: {
+                display: true,
+                title: {
+                    display: true,
+                    color: 'white',
+                },
+                ticks: {
+                    color: 'white',
+                    font: {
+                        family: 'Montserrat',
+                    },
+                },
+                grid: {
+                    color: '#FFFFFF20',
+                },
+                border: {
+                    color: '#FFFFFF20'
+                }
+            },
+            y: {
+                min: 0,
+                max: 100,
+                display: true,
+                title: {
+                    display: true,
+                    color: 'white'
+                },
+                ticks: {
+                    color: 'white',
+                    font: {
+                        family: 'Montserrat',
+                    },
+                },
+                grid: {
+                    color: '#FFFFFF20',
+                },
+                border: {
+                    color: '#FFFFFF20'
+                }
+            },
+        },
+    },
+});
---
+++ site/public/js/visaoEspecifica/tempoAtividade.js
@@ -0,0 +1,35 @@
+var ctx = document.getElementById('gaugeChart').getContext('2d');
+var canvas = document.getElementById('gaugeChart');
+canvas.height = window.innerHeight/4.5;
+
+var data = {
+    datasets: [{
+        data: [90, 10],
+        backgroundColor: [
+            '#DC4444',
+            'transparent'
+        ],
+        borderWidth: 0,
+        borderJoinStyle: 'round',
+        borderRadius: 25
+    }]
+};
+
+var options = {
+    circumference: Math.PI * 76,
+    rotation: 240,
+    cutout: "85%",
+    legend: {
+        display: false,
+    },
+    tooltips: {
+        enabled: false,
+    },
+    responsive: false,
+};
+
+var gaugeChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: data,
+    options: options,
+});
