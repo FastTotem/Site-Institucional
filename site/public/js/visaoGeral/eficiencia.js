062f0e5b8073416d524598f061c2eb8b6d141144
Kauã Leal Marcondes dos Santos <130408422+KauaLealz@users.noreply.github.com>
2023-10-11 16:59:43 -0300

    Implementação da aba listagem

--- site/public/js/visaoGeral/eficiencia.js
+++ site/public/js/visaoGeral/eficiencia.js
@@ -7,17 +7,17 @@ document.addEventListener('DOMContentLoaded', function() {
 var efficiencyCanvas = document.getElementById('efficiency-canvas').getContext('2d');
 
 var efficiencyData = {
-    labels: ['Totem 1', 'Totem 2', 'Totem 3', 'Totem 4'],
+    labels: ['Totem 1', 'Totem 2', 'Totem 3', 'Totem 4', 'Totem 5', 'Totem 6', 'Totem 7', 'Totem 8', 'Totem 9', 'Totem 10'],
     datasets: [
         {
-            data: [80, 65, 90, 75],
+            data: [80, 65, 90, 75, 20, 40, 50, 90, 10, 35],
             backgroundColor: 'rgba(0, 255, 0, 0.7)',
             borderColor: borderColorOk,
             borderWidth: 2,
             label: 'Funcionando',
         },
         {
-            data: [20, 35, 10, 25],
+            data: [20, 35, 10, 25, 80, 60, 50, 10, 90, 65],
             backgroundColor: 'rgba(255, 99, 71, 0.7)',
             borderColor: borderColorCritico,
             borderWidth: 2,
