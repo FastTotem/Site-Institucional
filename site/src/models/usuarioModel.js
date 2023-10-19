2ec0a5730f872be53d070ad97e87878cec9afaa2
Kauã Leal Marcondes dos Santos <130408422+KauaLealz@users.noreply.github.com>
2023-10-19 20:08:46 -0300

    Bugfix cadastro

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -3,33 +3,33 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
-    SELECT idUsuario, Nome, Email, NivelDeAcesso, fkEmpresa FROM usuario WHERE email = '${email}' AND senha = '${senha}';`;
+    SELECT idUsuario, nome, email, nivelAcesso, fkEmpresa FROM usuario WHERE email = '${email}' AND senha = '${senha}';`;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
 function checarSenha(id, senha) {
-    var instrucao = `SELECT * FROM usuario WHERE idUsuario = '${id}' AND senha = '${senha}';`;
+    var instrucao = `SELECT * FROM Usuario WHERE idUsuario = '${id}' AND senha = '${senha}';`;
     return database.executar(instrucao);
 }
 
 function getUser(id) {
-    var instrucao = `SELECT * FROM usuario WHERE idUsuario = '${id}';`;
+    var instrucao = `SELECT * FROM Usuario WHERE idUsuario = '${id}';`;
     return database.executar(instrucao);
 }
 
 function updateSenha(id, senha) {
-    var instrucao = `UPDATE usuario SET senha='${senha}' WHERE idUsuario = '${id}';`;
+    var instrucao = `UPDATE Usuario SET senha='${senha}' WHERE idUsuario = '${id}';`;
     return database.executar(instrucao);
 }
 
 function updateNome(id, nome) {
-    var instrucao = `UPDATE usuario SET nome='${nome}' WHERE idUsuario = '${id}';`;
+    var instrucao = `UPDATE Usuario SET nome='${nome}' WHERE idUsuario = '${id}';`;
     return database.executar(instrucao);
 }
 
 function cadastrar(nome, email, senha, empresaId, nivelDeAcesso) {
-    var usuarioQuery = `INSERT INTO Usuario (Nome, Email, Senha, NivelDeAcesso, fkEmpresa) VALUES ('${nome}Admin', '${email}', '${senha}', '${nivelDeAcesso}', ${empresaId})`;
+    var usuarioQuery = `INSERT INTO Usuario (nome, email, senha, nivelAcesso, fkEmpresa) VALUES ('${nome}Admin', '${email}', '${senha}', '${nivelDeAcesso}', ${empresaId})`;
 
     return database.executar(usuarioQuery);
 }
