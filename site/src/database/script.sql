2b0c2ee20f197ab840288e87015b9d138c3d7cd2
Kauã Leal Marcondes dos Santos <130408422+KauaLealz@users.noreply.github.com>
2023-10-25 20:12:27 -0300

    Update script.sql

--- site/src/database/script.sql
+++ site/src/database/script.sql
@@ -1,93 +1,95 @@
--- SQLBook: Code
-CREATE DATABASE fastTotem;
-USE fastTotem;
+	-- SQLBook: Code
 
-CREATE TABLE Endereco (
-	idEndereco INT PRIMARY KEY AUTO_INCREMENT,
-	logradouro VARCHAR(255),
-	bairro VARCHAR(255),
-	numero CHAR(7),
-	complemento VARCHAR(255),
-	cep VARCHAR(15)
-);
+	-- Verifica se o banco de dados 'fastTotem' existe
+	CREATE DATABASE IF NOT EXISTS fasttotem;
+	USE fasttotem;
 
-CREATE TABLE Empresa(
-	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
-	razaoSocial VARCHAR(120),
-	cnpj CHAR(14),
-	email VARCHAR(255),
-    	fkEndereco INT,
-	FOREIGN KEY (fkEndereco) REFERENCES Endereco(idEndereco)
-);
+	-- Cria a tabela Endereco se ela não existir
+	CREATE TABLE IF NOT EXISTS endereco (
+		idEndereco INT PRIMARY KEY AUTO_INCREMENT,
+		logradouro VARCHAR(255),
+		bairro VARCHAR(255),
+		numero CHAR(7),
+		complemento VARCHAR(255),
+		cep VARCHAR(15)
+	);
 
-CREATE TABLE Usuario (
-	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(255),
-	email VARCHAR(255),
-	senha VARCHAR(255),
-	nivelAcesso ENUM('Administrador','Funcionário'),
-	imgUsuario TEXT(255),
-    	ativo TINYINT,
-	fkEmpresa INT,
-	FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
-);
+	-- Cria a tabela Empresa se ela não existir
+	CREATE TABLE IF NOT EXISTS empresa (
+		idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+		razaoSocial VARCHAR(120),
+		cnpj CHAR(14),
+		email VARCHAR(255),
+		fkEndereco INT,
+		FOREIGN KEY (fkEndereco) REFERENCES endereco (idEndereco)
+	);
 
-CREATE TABLE Totem(
-	idTotem INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(255),
-	chaveDeAcesso VARCHAR(255),
-    	boardSerialNumber VARCHAR(255),
-	ativo TINYINT,
-	fkEmpresa INT,
-	FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
-);
+	-- Cria a tabela Usuario se ela não existir
+	CREATE TABLE IF NOT EXISTS usuario (
+		idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+		nome VARCHAR(255),
+		email VARCHAR(255),
+		senha VARCHAR(255),
+		nivelAcesso ENUM('Administrador', 'Funcionário'),
+		imgUsuario TEXT(255),
+		fkEmpresa INT,
+		FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
+	);
 
-CREATE TABLE InfoMaquina(
-	idInfoMaquina INT PRIMARY KEY AUTO_INCREMENT,
-	sistemaOperacional VARCHAR(200),
-	fabricante  VARCHAR(200),
-	nomeProcessador VARCHAR(200),
-	capacidadeRam DOUBLE,
-	capacidadeDisco DOUBLE,
-	fkTotem INT,
-	FOREIGN KEY (fkTotem) REFERENCES Totem(idTotem)
-);
+	-- Cria a tabela Totem se ela não existir
+	CREATE TABLE IF NOT EXISTS totem (
+		idTotem INT PRIMARY KEY AUTO_INCREMENT,
+		nome VARCHAR(255),
+		chaveDeAcesso VARCHAR(255),
+		boardSerialNumber VARCHAR(255),
+		fkEmpresa INT,
+		FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
+	);
 
-CREATE TABLE Componente(
-	idComponente INT PRIMARY KEY AUTO_INCREMENT,
-	nomeComponente VARCHAR(255),
-    	tipoComponente VARCHAR(255),
-	fkTotem INT,
-	FOREIGN KEY (fkTotem) REFERENCES Totem(idTotem)
-);
+	-- Cria a tabela InfoMaquina se ela não existir
+	CREATE TABLE IF NOT EXISTS infoMaquina (
+		idInfoMaquina INT PRIMARY KEY AUTO_INCREMENT,
+		sistemaOperacional VARCHAR(200),
+		fabricante VARCHAR(200),
+		nomeProcessador VARCHAR(200),
+		capacidadeRam DOUBLE,
+		capacidadeDisco DOUBLE,
+		fkTotem INT,
+		FOREIGN KEY (fkTotem) REFERENCES totem (idTotem)
+	);
 
-CREATE TABLE Captura (
-	idCaptura INT PRIMARY KEY AUTO_INCREMENT,
-	dataHora DATETIME,
-	tipo VARCHAR(255),
-	valor DOUBLE,
-	fkComponente INT,
-    	fkTotem INT,
-	FOREIGN KEY (fkComponente) REFERENCES Componente(idComponente),
-	FOREIGN KEY (fkTotem) REFERENCES Totem(idTotem)
-);
+	-- Cria a tabela Componente se ela não existir
+	CREATE TABLE IF NOT EXISTS componente (
+		idComponente INT PRIMARY KEY AUTO_INCREMENT,
+		nomeComponente VARCHAR(255),
+		tipoComponente VARCHAR(255),
+		fkTotem INT,
+		FOREIGN KEY (fkTotem) REFERENCES totem (idTotem)
+	);
 
-CREATE TABLE ParametroAlerta(
-	idParametroAlerta INT PRIMARY KEY AUTO_INCREMENT,
-	fkComponente INT,
-	ideal INT,
-	alerta INT,
-	critico INT,
-	notificacao INT,
-	FOREIGN KEY (fkComponente) REFERENCES Componente (idComponente)
-); 
+	-- Cria a tabela Captura se ela não existir
+	CREATE TABLE IF NOT EXISTS captura (
+		idCaptura INT PRIMARY KEY AUTO_INCREMENT,
+		dataHora DATETIME,
+		tipo VARCHAR(255),
+		valor DOUBLE,
+		fkComponente INT,
+		fkTotem INT,
+		FOREIGN KEY (fkComponente) REFERENCES componente (idComponente),
+		FOREIGN KEY (fkTotem) REFERENCES totem (idTotem)
+	);
 
--- CREATE USER fastTotemAdmin@localhost IDENTIFIED BY 'fasttotem123';
--- GRANT ALL PRIVILEGES ON FastTotem.* TO 'fastTotemAdmin'@'localhost';
--- FLUSH PRIVILEGES;
+	-- Cria a tabela ParametroAlerta se ela não existir
+	CREATE TABLE IF NOT EXISTS parametroAlerta (
+		idParametroAlerta INT PRIMARY KEY AUTO_INCREMENT,
+		fkComponente INT,
+		ideal INT,
+		alerta INT,
+		critico INT,
+		notificacao INT,
+		FOREIGN KEY (fkComponente) REFERENCES componente (idComponente)
+	);
 
-INSERT INTO endereco (logradouro, bairro, numero, complemento, cep) VALUES ('Avenida Hilário Pereira de Souza', 'Centro', '492', 'Piso 2', '06010170');
-INSERT INTO empresa (razaoSocial, cnpj, email, fkEndereco) VALUES ('King Hamburgueria', '12345678978945', 'kinghamburgueria@mail.com', 1);
-INSERT INTO totem (nome, chaveDeAcesso, ativo, fkEmpresa) VALUES ('Totem01', '12345678', 1, 1);
-
--- drop database fasttotem;
+	INSERT INTO endereco (logradouro, bairro, numero, complemento, cep) VALUES ('Avenida Hilário Pereira de Souza', 'Centro', '492', 'Piso 2', '06010170');
+	INSERT INTO empresa (razaoSocial, cnpj, email, fkEndereco) VALUES ('King Hamburgueria', '12345678978945', 'kinghamburgueria@mail.com', 1);
+	INSERT INTO totem (nome, chaveDeAcesso, fkEmpresa) VALUES ('Totem01', '1234', 1);
