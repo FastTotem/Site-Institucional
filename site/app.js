2db98c3af091d112835aba7aab4ac95185072a83
Kauã Leal Marcondes dos Santos <130408422+KauaLealz@users.noreply.github.com>
2023-11-01 18:37:09 -0300

    Conexão da aba totens com o banco

--- site/app.js
+++ site/app.js
@@ -11,7 +11,7 @@ var indexRouter = require("./src/routes/index");
 var empresaRouter = require("./src/routes/empresa");
 var usuarioRouter = require("./src/routes/usuario");
 var totemRouter = require("./src/routes/totem");
-var capturaRouter = require("./src/routes/captura");
+var funcionarioRouter = require("./src/routes/funcionario");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,6 +22,8 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/empresa", empresaRouter);
+app.use("/totem", totemRouter);
+app.use("/funcionario", funcionarioRouter);
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
--- site/public/js/listagem.js
+++ site/public/js/listagem.js
@@ -1,44 +1,163 @@
-function selectColor(buttonId) {
-    const statusButton = document.getElementById(buttonId);
+const criticalSrc = "./assets/aviso.png";
+const alertSrc = "./assets/sinal-de-alerta.png";
+const okSrc = "./assets/verificar.png";
+const offSrc = "./assets/desligar.png";
 
-            const bgndColor = window.getComputedStyle(statusButton).backgroundColor;
-            const borderColor = window.getComputedStyle(statusButton).borderColor;
-            switchColor(bgndColor, borderColor, buttonId);
+function listarTotens() {
+    fetch(`/totem/${sessionStorage.ID_EMPRESA}/listar`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        criarCardsTotem(data);
+    })
+    .catch(error => {
+        console.error('Erro ao buscar informações do totem:', error);
+    });
 }
 
-function switchColor(bgndColor, borderColor, buttonId) {
-    const criticalSrc = "./assets/aviso.png";
-    const alertSrc = "./assets/sinal-de-alerta.png";
-    const okSrc = "./assets/verificar.png";
-    const offSrc = "./assets/desligar.png";
+function listarTotensPorStatus(status) {
+    fetch(`/totem/${sessionStorage.ID_EMPRESA}/${status}/listar`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        criarCardsTotem(data);
+    })
+    .catch(error => {
+        console.error('Erro ao buscar informações do totem:', error);
+    });
+}
 
-    const cards = document.querySelectorAll('.list-card');
+function criarCardsTotem(totemData) {
+    const totemList = document.getElementById('list');
 
-    cards.forEach((card, index) => {
-        const cardBackgroundColor = window.getComputedStyle(card).backgroundColor;
-        const imgElement = card.querySelector('.img-status img');
-        card.style.backgroundColor = bgndColor;
+    while (totemList.firstChild) {
+        totemList.removeChild(totemList.firstChild);
+    }
 
-        switch (buttonId) {
-            case 'all':
-                const allSrcs = [criticalSrc, alertSrc, okSrc, offSrc]; // Adicione outros srcs se necessário
-                const randomSrc = allSrcs[Math.floor(Math.random() * allSrcs.length)];
-                imgElement.src = randomSrc;
+    totemData.forEach(totem => {
+        const card = document.createElement('div');
+        card.classList.add('list-card');
+
+        const imgStatus = document.createElement('div');
+        imgStatus.classList.add('img-status');
+        const img = document.createElement('img');
+
+        switch (totem.totemstatus) {
+            case 'alerta':
+                card.classList.add('status-alerta');
+                img.src = alertSrc;
                 break;
-            case 'danger':
-                imgElement.src = criticalSrc;
+            case 'critico':
+                card.classList.add('status-critico');
+                img.src = criticalSrc;
                 break;
-            case 'alert':
-                imgElement.src = alertSrc;
+            case 'inativo':
+                card.classList.add('status-inativo');
+                img.src = offSrc;
                 break;
             case 'ok':
-                imgElement.src = okSrc;
-                break;
-            case 'off':
-                imgElement.src = offSrc;
-                break;
-            default:
+                card.classList.add('status-ok');
+                img.src = okSrc;
                 break;
         }
+
+        imgStatus.appendChild(img);
+
+        const totemInfo = document.createElement('div');
+        totemInfo.classList.add('totem-info');
+
+        const totemName = document.createElement('div');
+        totemName.classList.add('totem-name');
+        const h3 = document.createElement('h3');
+        h3.textContent = totem.nome || 'Nome Desconhecido';
+        totemName.appendChild(h3);
+
+        const message = document.createElement('div');
+        message.classList.add('message');
+        const link = document.createElement('a');
+        link.href = 'visaoEspecifica.html';
+        link.textContent = 'Clique aqui para ver mais';
+        message.appendChild(link);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete-button');
+
+        const imgDelete = document.createElement('img');
+        imgDelete.src = './assets/trash.png';
+        imgDelete.alt = 'Excluir';
+        deleteButton.appendChild(imgDelete);
+        deleteButton.onclick = () => excluirTotem(totem.nome || 'Nome Desconhecido');
+
+        totemInfo.appendChild(totemName);
+        totemInfo.appendChild(message);
+        totemInfo.appendChild(deleteButton);
+
+        card.appendChild(imgStatus);
+        card.appendChild(totemInfo);
+
+        totemList.appendChild(card);
     });
+}
+
+function cadastrarTotem(nome, jar, chave) {
+
+var chaveDeAcesso;
+
+if(chave!=""){
+ chaveDeAcesso = chave;
+}else{
+    chaveDeAcesso = gerarChaveDeAcesso()
+}
+
+    const data = fetch(`/totem/cadastrar`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nomeServer: nome,
+            jarServer: jar,
+            chaveServer: chaveDeAcesso,
+            fkEmpresaServer: sessionStorage.ID_EMPRESA
+        })
+    });
+        alert('Totem cadastrado com sucesso!');
+        window.location.reload();
+}
+
+function excluirTotem(nome) {
+    const data = fetch(`/totem/excluir`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nomeServer: nome,
+            fkEmpresaServer: sessionStorage.ID_EMPRESA
+        })
+    });
+
+        alert('Totem deletado com sucesso!');
+        window.location.reload();
+}
+
+function gerarChaveDeAcesso() {
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const tamanhoChave = 10;
+    let chave = '';
+
+    for (let i = 0; i < tamanhoChave; i++) {
+        const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+        chave += caracteres.charAt(indiceAleatorio);
+    }
+
+    return chave;
 }
