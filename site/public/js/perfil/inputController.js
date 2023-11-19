0cd08d7781819606871d73153a397a2b1ada753d
Diego <diegosousa241104@gmail.com>
2023-11-18 23:49:53 -0300

    fix: corrigindo exibicao do perfil do usuario

--- site/public/js/perfil/inputController.js
+++ site/public/js/perfil/inputController.js
@@ -2,25 +2,25 @@ const confirmPasswordContainer = document.getElementById('confirmPasswordContain
 const modalPassword = document.getElementById('modalPassword');
 const profileImageInput = document.getElementById('profileImageInput');
 
-profileImageInput.addEventListener('change', (event) => {
-    const file = event.target.files[0];
+// profileImageInput.addEventListener('change', (event) => {
+//     const file = event.target.files[0];
 
-    if(file) {
-        const reader = new FileReader();
+//     if(file) {
+//         const reader = new FileReader();
         
-        reader.addEventListener('load', (e) => {
-            const image = e.target.result;
+//         reader.addEventListener('load', (e) => {
+//             const image = e.target.result;
             
-            const profileImageText = document.querySelector(".profileImage h1");
-            const profileImage = document.querySelector(".formContainer label");
+//             const profileImageText = document.querySelector(".profileImage h1");
+//             const profileImage = document.querySelector(".formContainer label");
 
-            profileImageText.style.display = "none";
-            profileImage.style.backgroundImage = `url(${image})`;
-        })
+//             profileImageText.style.display = "none";
+//             profileImage.style.backgroundImage = `url(${image})`;
+//         })
 
-        reader.readAsDataURL(file);
-    }
-});
+//         reader.readAsDataURL(file);
+//     }
+// });
 
 function toggleLock(containerTargetId) {
     const elementContainer = document.getElementById(containerTargetId);
@@ -30,7 +30,6 @@ function toggleLock(containerTargetId) {
     if(elementInput.disabled) {
         elementInput.disabled = false;
         elementLockIcon.dataset.icon = "material-symbols:lock-open";
-        console.log(elementLockIcon);
     } else {
         elementInput.disabled = true;
         elementLockIcon.dataset.icon = "material-symbols:lock";
